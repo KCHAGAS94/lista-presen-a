8b169586b605d80a94d56b310a56a295d072859d
kchagas94 <kenedychagas@gmail.com>
2025-06-23 23:50:43 -0300

    add-agenda

--- js/lista-firebase.js
+++ js/lista-firebase.js
@@ -1,13 +1,13 @@
 
-var firebaseConfig1 = {
-  apiKey: "AIzaSyDKOSdrF4lNvBacn8NC6kKBNWYWYuCw_VE",
-  authDomain: "lista-presenca-5e061.firebaseapp.com",
-  databaseURL: "https://lista-presenca-5e061-default-rtdb.firebaseio.com",
-  projectId: "lista-presenca-5e061",
-  storageBucket: "lista-presenca-5e061.firebasestorage.app",
-  messagingSenderId: "110179186838",
-  appId: "1:110179186838:web:7f7f77ef344310f3df6ab7"
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyDKOSdrF4lNvBacn8NC6kKBNWYWYuCw_VE",
+    authDomain: "lista-presenca-5e061.firebaseapp.com",
+    databaseURL: "https://lista-presenca-5e061-default-rtdb.firebaseio.com",
+    projectId: "lista-presenca-5e061",
+    storageBucket: "lista-presenca-5e061.firebasestorage.app",
+    messagingSenderId: "110179186838",
+    appId: "1:110179186838:web:7f7f77ef344310f3df6ab7"
+  };
 
-
-
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
